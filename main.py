657b45cd62049fed2a11ad462431ede221788701
Brandon Wong <brandonwong985@gmail.com>
2022-10-09 14:41:31 -0700

    restructure code for debug

--- main.py
+++ main.py
@@ -11,17 +11,6 @@ TWEET_TIMESTAMP = '0'
 MY_TIMEZONE = "America/Los_Angeles"
 punct = ["...", "?", "?!", "!", "~", "?..", "!!"]
 
-
-auth = tweepy.OAuthHandler(config.api_key, config.api_key_secret)
-auth.set_access_token(config.access_token, config.access_token_secret)
-
-api = tweepy.API(auth)
-try:
-    api.verify_credentials()
-    print("Authentication OK")
-except:
-    print("Error during authentication")
-
 def read_last_prime(FILE_NAME):
     file = open(FILE_NAME, 'r')
     last_prime = int(file.read().strip())
@@ -42,20 +31,31 @@ def get_next_prime():
 def tweet_prime_number():
     prime = get_next_prime()
     tweet = str(prime) + random.choice(punct)   
-    t = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    t = datetime.now().strftime("%m/%d/%Y %H:%M:%S")
     try:
         api.update_status(tweet)
         print("tweeted " + tweet + " at " + t)  
         write_last_prime(PRIME_FILE, prime)  
     except:
-        tweet_failed(DEBUG_FILE, prime, t)
+        msg = "tweet " + str(prime) + " failed at " + str(t)
+        log_debug(DEBUG_FILE, msg)
 
-def tweet_failed(FILE_NAME, failed_prime, t):
+def log_debug(FILE_NAME, msg):
     file = open(FILE_NAME, 'a')
-    file.write("tweet " + str(failed_prime) + " failed at " + str(t))
+    file.write(msg)
     file.close()
     return
 
+auth = tweepy.OAuthHandler(config.api_key, config.api_key_secret)
+auth.set_access_token(config.access_token, config.access_token_secret)
+
+api = tweepy.API(auth)
+try:
+    api.verify_credentials()
+    print("Authentication OK")
+except:
+    msg = "Error during authentication at " + str(datetime.now().strftime("%m/%d/%Y %H:%M:%S"))
+    log_debug(DEBUG_FILE, msg)
 
 def main():
     sched = BlockingScheduler(timezone=MY_TIMEZONE)

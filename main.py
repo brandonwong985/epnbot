76bc997d0a8c7d579973263077322bd0a70e16f8
Brandon Wong <brandonwong985@gmail.com>
2022-10-09 06:00:57 -0700

    added file reader functions

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tweepy
 import config
+from sympy import *
 
 auth = tweepy.OAuthHandler(config.api_key, config.api_key_secret)
 auth.set_access_token(config.access_token, config.access_token_secret)
@@ -12,3 +13,15 @@ except:
     print("Error during authentication")
 
 #api.update_status("updating status using tweepy!")
+
+def read_last_prime(FILE_NAME):
+    file = open(FILE_NAME, 'r')
+    last_prime = int(file.read().strip())
+    file.close()
+    return last_prime
+
+def write_last_prime(FILE_NAME, last_prime):
+    file = open(FILE_NAME, 'w')
+    file.write(str(last_prime))
+    file.close()
+    return

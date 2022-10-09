9528feec19f785244daeb135e7e213cdec75bc05
Brandon Wong <brandonwong985@gmail.com>
2022-10-09 14:49:43 -0700

    updated write prime function

--- main.py
+++ main.py
@@ -7,18 +7,18 @@ from datetime import datetime
 
 PRIME_FILE = "prime.txt"
 DEBUG_FILE = "debug.txt"
-TWEET_TIMESTAMP = '45'
+TWEET_TIMESTAMP = '0'
 MY_TIMEZONE = "America/Los_Angeles"
 punct = ["", "...", "?", "?!", "!", "~", "?..", "!!"]
 
 def read_last_prime(FILE_NAME):
     file = open(FILE_NAME, 'r')
-    last_prime = int(file.read().strip())
+    last_prime = int(file.readlines()[-1])
     file.close()
     return last_prime
 
 def write_last_prime(FILE_NAME, last_prime):
-    file = open(FILE_NAME, 'w')
+    file = open(FILE_NAME, 'a')
     file.write(str(last_prime))
     file.close()
     return
